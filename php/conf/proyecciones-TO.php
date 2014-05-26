97daee72a5fd983cf25967cfbb4e109fada116ac
xabiercid <xabier@xabiercid.eu>
2014-05-26 11:45:01 +0100

    list of provinces

--- php/conf/proyecciones-TO.php
+++ php/conf/proyecciones-TO.php
@@ -1,78 +1,359 @@
 <?php
 return [
-    '09' => [
-        'nombre' => 'Cataluña',
+    '01' => [
+        'nombre' => 'Álava',
         'limite' => 3,
-        'provincias' => [
-            '08' => [
-                'nombre' => 'Barcelona',
-                'escanos' => 85
+        'escanos' => 4
             ],
-            '17' => [
-                'nombre' => 'Girona',
-                'escanos' => 20
-            ],
-            '25' => [
-                'nombre' => 'Lleida',
-                'escanos' => 15
-            ],
-            '43' => [
-                'nombre' => 'Tarragona',
-                'escanos' => 15
-            ]
-        ]
-    ],
-
-    '14' => [
-        'nombre' => 'País Vasco',
-        'limite' => 3,
-        'provincias' => [
-            '48' => [
-                'nombre' => 'Bizkaia',
-                'escanos' => 25
-            ],
-            '20' => [
-                'nombre' => 'Gipuzkoa',
-                'escanos' => 25
-            ],
-            '01' => [
-                'nombre' => 'Araba / Álava',
-                'escanos' => 25
-            ],
-        ]
-    ],
-
-    '11' => [
-        'nombre' => 'Galicia',
-        'limite' => 5,
-        'provincias' => [
-            '15' => [
-                'nombre' => 'A Coruña',
-                'escanos' => 24
-            ],
-            '27' => [
-                'nombre' => 'Lugo',
-                'escanos' => 15
-            ],
-            '32' => [
-                'nombre' => 'Ourense',
-                'escanos' => 14
-            ],
-            '36' => [
-                'nombre' => 'Pontevedra',
-                'escanos' => 22
-            ],
-        ]
-    ],
-
-    '12' => [
-        'nombre' => 'Madrid',
-        'limite' => 5,
-        'provincias' => [
-            '28' => [
-                'nombre' => 'Madrid',
-                'escanos' => 129
-            ]
-        ]
     ]
+    [
+    '02' => [
+        'nombre' => 'Albacete',
+        'limite' => 3,
+        'escanos' => 4
+            ],
+    ]
+    [
+    '03' => [
+        'nombre' => 'Alicante',
+        'limite' => 3,
+        'escanos' => 12
+            ],
+	]
+	[
+    '04' => [
+        'nombre' => 'Almería',
+        'limite' => 3,
+        'escanos' => 6
+            ],
+	]
+	[
+    '05' => [
+        'nombre' => 'Ávila',
+        'limite' => 3,
+        'escanos' => 3
+            ],
+	]
+	[
+    '06' => [
+        'nombre' => 'Badajoz',
+        'limite' => 3,
+        'escanos' => 6
+            ],
+	]
+	[
+    '07' => [
+        'nombre' => 'Illes Balears',
+        'limite' => 3,
+        'escanos' => 8
+            ],
+	]
+	[
+    '08' => [
+        'nombre' => 'Barcelona',
+        'limite' => 3,
+        'escanos' => 31
+            ],
+	]
+	[
+    '09' => [
+        'nombre' => 'Burgos',
+        'limite' => 3,
+        'escanos' => 4
+            ],
+	]
+	[
+    '10' => [
+        'nombre' => 'Cáceres',
+        'limite' => 3,
+        'escanos' => 4
+            ],
+	]
+	[
+    '11' => [
+        'nombre' => 'Cádiz',
+        'limite' => 3,
+        'escanos' => 8
+            ],
+	]
+	[
+    '12' => [
+        'nombre' => 'Castellón',
+        'limite' => 3,
+        'escanos' => 5
+            ],
+	]
+	[
+    '13' => [
+        'nombre' => 'Ciudad Real',
+        'limite' => 3,
+        'escanos' => 5
+            ],
+	]
+	[
+    '14' => [
+        'nombre' => 'Córdoba',
+        'limite' => 3,
+        'escanos' => 6
+            ],
+	]
+	[
+    '15' => [
+        'nombre' => 'A Coruña',
+        'limite' => 3,
+        'escanos' => 8
+            ],
+	]
+	[
+    '16' => [
+        'nombre' => 'Cuenca',
+        'limite' => 3,
+        'escanos' => 3
+            ],
+	]
+	[
+    '17' => [
+        'nombre' => 'Girona',
+        'limite' => 3,
+        'escanos' => 6
+            ],
+	]
+	[
+    '18' => [
+        'nombre' => 'Granada',
+        'limite' => 3,
+        'escanos' => 7
+            ],
+	]
+	[
+    '19' => [
+        'nombre' => 'Guadalajara',
+        'limite' => 3,
+        'escanos' => 3
+            ],
+	][
+    '20' => [
+        'nombre' => 'Gipuzkoa',
+        'limite' => 3,
+        'escanos' => 6
+            ],
+	]
+	[
+    '21' => [
+        'nombre' => 'Huelva',
+        'limite' => 3,
+        'escanos' => 5
+            ],
+	]
+	[
+    '22' => [
+        'nombre' => 'Huesca',
+        'limite' => 3,
+        'escanos' => 3
+            ],
+	]
+	[
+    '23' => [
+        'nombre' => 'Jaén',
+        'limite' => 3,
+        'escanos' => 6
+            ],
+	]
+	[
+    '24' => [
+        'nombre' => 'León',
+        'limite' => 3,
+        'escanos' => 5
+            ],
+	][
+    '25' => [
+        'nombre' => 'Lleida',
+        'limite' => 3,
+        'escanos' => 4
+            ],
+	]
+	[
+    '26' => [
+        'nombre' => 'Logroño',
+        'limite' => 3,
+        'escanos' => 4
+            ],
+	]
+	[
+    '27' => [
+        'nombre' => 'Lugo',
+        'limite' => 3,
+        'escanos' => 4
+            ],
+	][
+    '28' => [
+        'nombre' => 'Madrid',
+        'limite' => 3,
+        'escanos' => 36 
+            ],
+	]
+	[
+    '29' => [
+        'nombre' => 'Málaga',
+        'limite' => 3,
+        'escanos' => 10
+            ],
+	]
+	[
+    '30' => [
+        'nombre' => 'Murcia',
+        'limite' => 3,
+        'escanos' => 10
+            ],
+	]
+	[
+    '31' => [
+        'nombre' => 'Navarra',
+        'limite' => 3,
+        'escanos' => 5
+            ],
+	]
+	[
+    '32' => [
+        'nombre' => 'Ourense',
+        'limite' => 3,
+        'escanos' => 4
+            ],
+	]
+	[
+    '33' => [
+        'nombre' => 'Oviedo',
+        'limite' => 3,
+        'escanos' => 8
+            ],
+	]
+	[
+    '34' => [
+        'nombre' => 'Palencia',
+        'limite' => 3,
+        'escanos' => 3
+            ],
+	]
+	[
+    '35' => [
+        'nombre' => 'Las Palmas',
+        'limite' => 3,
+        'escanos' => 8
+            ],
+	]
+	[
+    '36' => [
+        'nombre' => 'Pontevedra',
+        'limite' => 3,
+        'escanos' => 7
+            ],
+	]
+	[
+    '37' => [
+        'nombre' => 'Salamanca',
+        'limite' => 3,
+        'escanos' => 4
+            ],
+	][
+    '38' => [
+        'nombre' => 'Tenerife',
+        'limite' => 3,
+        'escanos' => 7
+            ],
+	]
+	[
+    '39' => [
+        'nombre' => 'Santander',
+        'limite' => 3,
+        'escanos' => 5
+            ],
+	]
+	[
+    '40' => [
+        'nombre' => 'Segovia',
+        'limite' => 3,
+        'escanos' => 3
+            ],
+	][
+    '41' => [
+        'nombre' => 'Sevilla',
+        'limite' => 3,
+        'escanos' => 12
+            ],
+	]
+	[
+    '42' => [
+        'nombre' => 'Soria',
+        'limite' => 3,
+        'escanos' => 2
+            ],
+	][
+    '43' => [
+        'nombre' => 'Tarragona',
+        'limite' => 3,
+        'escanos' => 6
+            ],
+	]
+	[
+    '44' => [
+        'nombre' => 'Teruel',
+        'limite' => 3,
+        'escanos' => 3
+            ],
+	]
+	[
+    '45' => [
+        'nombre' => 'Toledo',
+        'limite' => 3,
+        'escanos' => 6
+            ],
+	][
+    '46' => [
+        'nombre' => 'Valencia',
+        'limite' => 3,
+        'escanos' => 16
+            ],
+	]
+	[
+    '47' => [
+        'nombre' => 'Valladolid',
+        'limite' => 3,
+        'escanos' => 5
+            ],
+	]
+	[
+    '48' => [
+        'nombre' => 'Bizkaia',
+        'limite' => 3,
+        'escanos' => 8
+            ],
+	]
+	[
+    '49' => [
+        'nombre' => 'Zamora',
+        'limite' => 3,
+        'escanos' => 3
+            ],
+	]
+	[
+    '50' => [
+        'nombre' => 'Zaragoza',
+        'limite' => 3,
+        'escanos' => 7
+            ],
+	]
+	[
+    '51' => [
+        'nombre' => 'Ceuta',
+        'limite' => 3,
+        'escanos' => 1
+            ],
+	]
+	[
+    '52' => [
+        'nombre' => 'Melilla',
+        'limite' => 3,
+        'escanos' => 1
+            ],
+	]
 ];
